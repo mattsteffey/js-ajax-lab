92785bd817f8d8f740b826f84b26ce241187eadb
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-18 19:58:09 -0700

    done, sucka

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -2,15 +2,44 @@ $( document ).ready(function() {
    console.log( "ready!" );
 });
 
-var ajax = $.get('https://den-super-crud.herokuapp.com/books/')
-   .done(function(data){
-       for (i=0; i<data.books.length; i++){
-           $('#books').append($('<img>',{src: data.books[i].image}));
-           $('#books').append($('<li>').text(data.books[i].title));
-           $('#books').append($('<li>').text(data.books[i].author));
-           $('#books').append($('<li>').text(data.books[i].releaseDate));
-       }
+var ajax = 
+	$.get('https://den-super-crud.herokuapp.com/books/')
+   		.done(function(data){
+       		for (i=0; i<data.books.length; i++){
+          		$('#books').append($('<img>',{src: data.books[i].image}));
+          		$('#books').append($('<li>').text(data.books[i].title));
+           		$('#books').append($('<li>').text(data.books[i].author));
+          		$('#books').append($('<li>').text(data.books[i].releaseDate));
+		    }
 
    });
 
+
+var formdata = 
+$("form#new-book").submit(function() {
+	event.preventDefault();	
+	var newImage = $("#book-image").val();
+	var newTitle = $("#book-title").val();
+	var newAuthor = $("#book-author").val();
+	var newDate = $("#release-date").val();
+	var list = $("ul#books");
+		list.append("<img src=" + newImage + "></img>");
+		list.append("<li>" + newTitle + "</li>");
+		list.append("<li>" + newAuthor + "</li>");
+		list.append("<li>" + newDate + "</li>");
+    
+
+    $.ajax({ 
+        type: 'POST', 
+        url: 'https://den-super-crud.herokuapp.com/books',
+        data: {
+        	"image": newImage,
+            "title": newTitle,
+            "author": newAuthor,
+            "releaseDate": newDate,
+        },
+        dataType: 'JSON'
+    }); 
+
+});
 
