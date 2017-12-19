dd38d1158b814bae19885543c5a546cffa71ab5c
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-12-18 18:59:47 -0700

    First part

---
+++ starter-code/app/app.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+   console.log( "ready!" );
+});
+
+var ajax = $.get('https://den-super-crud.herokuapp.com/books/')
+   .done(function(data){
+       for (i=0; i<data.books.length; i++){
+           $('#books').append($('<img>',{src: data.books[i].image}));
+           $('#books').append($('<li>').text(data.books[i].title));
+           $('#books').append($('<li>').text(data.books[i].author));
+           $('#books').append($('<li>').text(data.books[i].releaseDate));
+       }
+
+   });
+
+
